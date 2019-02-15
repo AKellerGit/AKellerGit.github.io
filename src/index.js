5d1c5975a18a7e1b49e741d3e5f281ebb5382503
AkellerGit <abkgit@gmail.com>
2019-02-15 14:34:04 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Hashrouter, Route, Link, Switch} from 'react-router-dom';
 
 //COMPONENTS
 import Header from './components/header';
+
+import Page1 from "./Page1";
+import Page2 from "./Page2";
+
+export default () => (
+  <HashRouter>
+    <Switch>
+      <Route exact path="/" component={Page1} />
+      <Route exact path="/" component={Page2} />
+    </Switch>
+  </HashRouter>
+)
 
 const App = () => {
   return <div><Header/></div>
