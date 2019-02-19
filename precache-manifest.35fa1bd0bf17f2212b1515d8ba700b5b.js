e396494b0a87613e6799bff8e5306efe2adf3449
AkellerGit <abkgit@gmail.com>
2019-02-19 18:28:43 -0500

    Updates

---
+++ precache-manifest.35fa1bd0bf17f2212b1515d8ba700b5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/portfolio/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "1628f77385e268c99090",
+    "url": "/portfolio/static/js/main.1628f773.chunk.js"
+  },
+  {
+    "revision": "a8b4456b15cf1b9e5059",
+    "url": "/portfolio/static/js/2.a8b4456b.chunk.js"
+  },
+  {
+    "revision": "1628f77385e268c99090",
+    "url": "/portfolio/static/css/main.70243af1.chunk.css"
+  },
+  {
+    "revision": "9e71992e2160a47239673df0ffa925eb",
+    "url": "/portfolio/index.html"
+  }
+];
